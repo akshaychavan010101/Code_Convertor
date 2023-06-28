584d380938d771c59d83b3e7191b57cee209acc0
akshaychavan010101 <akshaychavan010101@gmail.com>
2023-06-28 20:33:04 +0530

    final code to be deployed on cyclic

--- index.js
+++ index.js
@@ -14,11 +14,49 @@ const openai = new OpenAIApi(configuration);
 function generatePrompt(code, language, mode) {
   let prompt = "";
   if (mode === "convert") {
-    prompt = `Act as a software developer and Convert the following code to ${language}: \n\n${code}`;
+    prompt = `Act as a software developer and Convert the following code to ${language}: \n\n${code}\n\nAdd relevant inline comments to explain the code and its functionality.`;
   } else if (mode === "debug") {
-    prompt = `Act as a software developer and debug the following code: \n\n${code}\nProvide correct code and also provide the errors in the form of bullet points.`;
+    prompt = `You are a software developer working on a codebase and you encounter a bug in the code that needs debugging. The code you are debugging is as follows:
+    ---
+    // Code to be debugged
+    \n\n${code}
+    // ...
+    ---
+    You need to identify and fix the bug in the code. Can you help me debug it?
+    Debugging Steps:
+    1. Read and understand the code to identify any potential issues.
+    2. Look for any syntax errors, logical errors, or unexpected behavior.
+    3. Use debugging techniques to narrow down the problem area.
+    4. Make necessary changes to fix the bug and improve the code.
+    
+    Please provide step-by-step instructions on how to debug the code and resolve the errors. Clearly state the errors you identified and how you resolved them. Additionally, provide any refactoring suggestions to improve the code structure or performance.
+    
+    ---
+    Summary of Resolved Errors:
+    1. Error 1: [Description of the error]
+       - Resolution: [Explanation of how the error was resolved]
+    
+    2. Error 2: [Description of the error]
+       - Resolution: [Explanation of how the error was resolved]
+    
+    3. ...
+    ---
+    Note: Please make sure to provide detailed and clear instructions for debugging the code and resolving the errors.
+    `;
   } else if (mode === "checkQuality") {
-    prompt = `Act as a software developer and check the quality of the following code: \n\n${code}\nProvide the time complexity, space comlexity, understandability, clearity and also provide the errors all these statistics should have good explaination and also have some percentages for the above domains to get more comprehensions.`;
+    prompt = `Please provide a code quality assessment for the given code:\n\n${code}\n. Consider the following parameters:
+    1. Code Consistency: Evaluate the code for consistent coding style, naming conventions, and formatting.
+    2. Code Performance: Assess the code for efficient algorithms, optimized data structures, and overall performance considerations.
+    3. Code Documentation: Review the code for appropriate comments, inline documentation, and clear explanations of complex logic.
+    4. Error Handling: Examine the code for proper error handling and graceful error recovery mechanisms.
+    5. Code Testability: Evaluate the code for ease of unit testing, mocking, and overall testability.
+    6. Code Modularity: Assess the code for modular design, separation of concerns, and reusability of components.
+    7. Code Complexity: Analyze the code for excessive complexity, convoluted logic, and potential code smells.
+    8. Code Duplication: Identify any code duplication and assess its impact on maintainability and readability.
+    9. Code Readability: Evaluate the code for readability, clarity, and adherence to coding best practices.
+    
+    Please provide a summary of the code quality assessment and a report showing the percentage-wise evaluation for each parameter mentioned above.
+    `;
   }
   return prompt;
 }
